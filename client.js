2b9ed741caf64c1e43b3d427deac7624fecf0c8b
Yahye Mohamud <jyahye375@gmail.com>
2024-01-25 21:02:53 -0600

    changed

--- client.js
+++ client.js
@@ -6,8 +6,14 @@
  * 3. After the loop, log out the total number of
  *    hobbies
  */
+ let hobbies = ['basketball', 'coding', 'hockey'];
+ console.log(hobbies);
+ console.log('for of loop');
 
-
+ for (let i = 0;  i < hobbies.length; i+=1){
+     console.log(hobbies[i]);
+ }
+ console.log(hobbies.length);
 // Example output
 // 1. swimming
 // 2. board games
@@ -24,6 +30,14 @@
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
+let colors = ['green', 'red', 'teal', 'orange'];
+tealCount = 0;
+let index = colors.length -1;
+for(let tealCount= 0; tealCount <  colors.length;  tealCount += 1) {
+     console.log('This is teal')
+     console.log('tealcount', tealCount);
+
+}
 
 
 // Example output
@@ -40,6 +54,18 @@
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
+numbers = [1, 2, 3, 4, 5];
+evenNumbers = [];
+oddNumbers = [];
+
+//if numbers%2===0 
+
+
+console.log('original array: ', numbers);
+console.log('Even Numbers: ', numbers);
+console.log('Odd Numbers: ', numbers);
+
+
 
 
 // Example output
@@ -56,8 +82,18 @@
  * 3. Write a loop that adds the opposite value to the toggled array
  * 4. Output both arrays
  */
-
-
+let Captain = [true, false, true, false];
+let toggled = [];
+for(let david of Captain){
+     if (Captain === true){
+        toggled.push(false)
+     }
+     else{
+          toggled.push(true)
+     }
+}
+console.log(Captain);
+console.log(toggled);
 // Example output
 // true, false, true, true
 // Toggled false, true, false, false
@@ -74,8 +110,11 @@
  *
  * Hint: Try using a while loop for this one.
  */
-
-
+let pew = [2, 0, 4, 5, 0, 0, 0, 0];
+while (pew.length > 0 && pew[pew.length -1] === 0){
+     pew.pop();
+}
+console.log(pew)
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
 // After loop 3, 0, 2, 8
@@ -90,8 +129,21 @@
       and lowest numbers.
  * 3. Log the highest and lowest number to the console
  */
-
-
+      donut = [3, 6, 2, 56, 32, 5, 89, 32];
+      console.log(donut);
+      High = donut[0];
+      low = donut[0];
+      
+      for (i = 0; i < donut.length; i++) {
+      if (donut[i] > High ) {
+      High= donut[i];
+      }
+      if (donut[i] < low ) {
+          low = donut[i];
+      }
+     }
+      console.log('High:', High);
+      console.log ('Low:', low);
 // Example output
 // 2, 2, -3, 7, 4, 1, 7, 12, 8
 // High: 12
